9ae83791783286eb930f033917063628d08d35f2
DilanTharaka <tharakad049@gmail.com>
2022-10-05 14:08:25 +0530

    update backend user and vehicle classes

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 8900
 
 const vehicle = require('./routes/vehicles')
 const users = require('./routes/users')
@@ -10,6 +10,18 @@ app.use(express.json())
 app.use('/vehicle', vehicle)
 app.use('/users', users)
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+}
+
+mongoose.connect(options)
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- backEnd/routes/vehicles.js
+++ backEnd/routes/vehicles.js
@@ -17,60 +17,60 @@ connection.connect(function (err) {
         })
     }
 })
-router.post('/', (req, res) => {
-    const code = req.body.code
-    const vehicleName = req.body.vehicleName
-    const vehicleImage = req.body.vehicleImage
-    const price = req.body.price
-    var saveVehicleQuery = "INSERT INTO vehivle(code,vehicleName,vehicleImage,price) VALUES(?,?,?,?)";
-    connection.query(saveVehicleQuery, [code, vehicleName, vehicleImage, price], (err) => {
-        if (err) {
-            res.send({ "message": "duplicate entry" })
-        } else {
-            res.send({ "message": "vehivle saved" })
-        }
-    })
-})
-router.get('/', (req, res) => {
-    var getAllVehicleQuery = "SELECT * FROM vehicle";
-    connection.query(getAllVehicleQuery, (err, rows) => {
-        if (err) console.log(err);
-        res.send(rows);
-    })
-})
-router.get('/:code',(req,res)=>{
-    const code = req.params.code
-    var searchVehicleQuery = "SELECT * FROM vehicle WHERE code=?"
-    connection.query(searchVehicleQuery, [code], (err, row) => {
-        if (err) console.log(err);
-        res.send(row);
-    })
-})
-router.put('/', (req, res) => {
-    const code = req.body.code
-    const vehicleName = req.body.vehicleName
-    const vehicleImage = req.body.vehicleImage
-    const price = req.body.price
-    var updateVehicleQuery = "UPDATE vehicle SET vehicleName=?, vehicleImage=?, price=? WHERE code=?";
-    connection.query(updateVehicleQuery, [vehicleName, vehicleImage, price, code], (err, rows) => {
-        if (err) console.log(err);
-        if (rows.affectedRows > 0) {
-            res.send({ "message": "vehicle updated" })
-        } else {
-            res.send({ "message": "vehicle not found" })
-        }
-    })
-})
-router.delete('/:code', (req, res) => {
-    const code = req.params.code
-    var deleteVehicleQuery = "DELETE FROM vehicle WHERE code=?";
-    connection.query(deleteVehicleQuery, [code], (err, rows) => {
-        if (err) console.log(err);
-        if (rows.affectedRows > 0) {
-            res.send({ "message": "vehicle deleted" })
-        } else {
-            res.send({ "message": "vehicle not found" })
-        }
-    })
-})
+// router.post('/', (req, res) => {
+//     const code = req.body.code
+//     const vehicleName = req.body.vehicleName
+//     const vehicleImage = req.body.vehicleImage
+//     const price = req.body.price
+//     var saveVehicleQuery = "INSERT INTO vehivle(code,vehicleName,vehicleImage,price) VALUES(?,?,?,?)";
+//     connection.query(saveVehicleQuery, [code, vehicleName, vehicleImage, price], (err) => {
+//         if (err) {
+//             res.send({ "message": "duplicate entry" })
+//         } else {
+//             res.send({ "message": "vehivle saved" })
+//         }
+//     })
+// })
+// router.get('/', (req, res) => {
+//     var getAllVehicleQuery = "SELECT * FROM vehicle";
+//     connection.query(getAllVehicleQuery, (err, rows) => {
+//         if (err) console.log(err);
+//         res.send(rows);
+//     })
+// })
+// router.get('/:code',(req,res)=>{
+//     const code = req.params.code
+//     var searchVehicleQuery = "SELECT * FROM vehicle WHERE code=?"
+//     connection.query(searchVehicleQuery, [code], (err, row) => {
+//         if (err) console.log(err);
+//         res.send(row);
+//     })
+// })
+// router.put('/', (req, res) => {
+//     const code = req.body.code
+//     const vehicleName = req.body.vehicleName
+//     const vehicleImage = req.body.vehicleImage
+//     const price = req.body.price
+//     var updateVehicleQuery = "UPDATE vehicle SET vehicleName=?, vehicleImage=?, price=? WHERE code=?";
+//     connection.query(updateVehicleQuery, [vehicleName, vehicleImage, price, code], (err, rows) => {
+//         if (err) console.log(err);
+//         if (rows.affectedRows > 0) {
+//             res.send({ "message": "vehicle updated" })
+//         } else {
+//             res.send({ "message": "vehicle not found" })
+//         }
+//     })
+// })
+// router.delete('/:code', (req, res) => {
+//     const code = req.params.code
+//     var deleteVehicleQuery = "DELETE FROM vehicle WHERE code=?";
+//     connection.query(deleteVehicleQuery, [code], (err, rows) => {
+//         if (err) console.log(err);
+//         if (rows.affectedRows > 0) {
+//             res.send({ "message": "vehicle deleted" })
+//         } else {
+//             res.send({ "message": "vehicle not found" })
+//         }
+//     })
+// })
 module.exports = router;
